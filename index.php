880ed7a5619e2860f9c86dce68b0ac425437c89c
evaldo <evaldo.junior@etec.sp.gov.br>
2023-08-29 13:51:54 -0300

    sistema de templates

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 require_once __DIR__ . '/Router.php';
 $request = $_SERVER['REQUEST_URI'];
+
+
+$template = new Template();
+$template->header();
 (new Router())->run($request);
+$template->footer();
