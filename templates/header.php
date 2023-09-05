811a60aa9e50d00f80604129d0db01638ba27828
evaldo <evaldo.junior@etec.sp.gov.br>
2023-09-05 13:54:38 -0300

    Página empresa.

--- templates/header.php
+++ templates/header.php
@@ -14,7 +14,7 @@
 
             <ul class="navbar-nav mb-2 mb-lg-0 ">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Empresas</a>
+                    <a class="nav-link" href="empresa">Empresas</a>
                 </li>
             </ul>
         </div>
