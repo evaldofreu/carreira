3011f99afb9f4aa0ef2accb80f135f0cbe6d4e88
evaldo <evaldo.junior@etec.sp.gov.br>
2023-09-12 13:16:08 -0300

    Registros de teste.

--- pages/empresa/empresa_controller.php
+++ pages/empresa/empresa_controller.php
@@ -5,7 +5,29 @@
 
      }
      function listar(){
-
+        $empresas = [];
+        $empresa = new Empresa();
+        $empresa->setId(1);
+        $empresa->setNome("ETEC PEDRO BADRAN");
+        $empresa->setTelefone("163818-2122");
+        $empresa->setGeolocalizacao("-20.5886455,-47.8621156");
+        $empresa->setCidadeIbge(3549409);
+        array_push($empresas,$empresa);
+        $empresa = new Empresa();
+        $empresa->setId(2);
+        $empresa->setNome("VENTUROSO VALENTINI");
+        $empresa->setTelefone("163818-5454");
+        $empresa->setGeolocalizacao("-20.5628803,-47.8587687");
+        $empresa->setCidadeIbge(3549409);
+        array_push($empresas,$empresa);
+        $empresa = new Empresa();
+        $empresa->setId(3);
+        $empresa->setNome("USINA ALTA MOGIANA");
+        $empresa->setTelefone("163810-1000");
+        $empresa->setGeolocalizacao("-20.4838359,-47.8761024");
+        $empresa->setCidadeIbge(3549409);
+        array_push($empresas,$empresa);
+        return $empresa;
      }
      function mostrar($id){
 
