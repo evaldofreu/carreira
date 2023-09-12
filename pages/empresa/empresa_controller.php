6c4b7ebe670dd38297e45cd0c39ec005944a725d
evaldo <evaldo.junior@etec.sp.gov.br>
2023-09-12 13:04:54 -0300

    Controlador de empresa

---
+++ pages/empresa/empresa_controller.php
@@ -0,0 +1,22 @@
+<?
+ require_once "../dominio/empresa.php";
+ class EmpresaController {
+     function incluir(Empresa $empresa){
+
+     }
+     function listar(){
+
+     }
+     function mostrar($id){
+
+     }
+     function atualizar(Empresa $empresa){
+
+     }
+
+     function deletar($id){
+
+     }
+
+
+}
