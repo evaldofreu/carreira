fc9ec1f92a397dfd161a1490d6e087153c1597ba
evaldo <evaldo.junior@etec.sp.gov.br>
2023-10-31 13:45:37 -0300

    Rotina para carregar estados do IBGE e campo para selecionar o estado.

--- pages/empresa/novo.php
+++ pages/empresa/novo.php
@@ -6,11 +6,19 @@
           <!--Incluir Atributo "name" nos inputs que vão ser enviados para salvar no banco de dados -->        
           <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome da empresa">
         </div>
+        
+        
+        <div class="mb-3 input-group">
+        <select class="form-select" aria-label="Default select example" id="estados">
+            <option selected>Selecione um estado</option>
+        </select>
+        </div>            
 
 
         <div class="mb-3 input-group">
+            <span class="input-group-text">Estado</span>
             <!--Incluir os inputs que serão utilizados para enviar a geolocalização  -->        
-            <input type="hidden" id="geolocalizacao" name="geolocalizacao" >
+            <input type="hidden" id="geolocalizacao" name="geolocalizacao" onchange="carregarCidadesIBGE()">
 
             <span class="input-group-text">Endereço</span>
             <input type="text" class="form-control" id="endereco" placeholder="Digite o endereço da empresa">
@@ -25,4 +33,37 @@
         
     </form>    
 </div>
+<script>
+     function carregarEstadosIBGE(){
+        let url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                    if (data!=null && data.length>0) {
 
+                        
+                        var selectEstado = document.getElementById("estados");
+                        data.forEach(element => {
+                            
+                            let option = document.createElement('option');
+                            option.value= element['sigla'];
+                            option.innerText = element['nome'];
+                            selectEstado.appendChild(option);
+
+                        });
+
+
+                    }
+            }).catch(error => {
+                    console.log("Erro carregando estados "+error);
+                });;
+     } 
+
+
+     carregarEstadosIBGE();
+
+
+     function carregarCidadesIBGE(){
+        
+     }
+</script>    
