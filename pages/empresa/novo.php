983457e7fafa9b4fc67cc6f6d466b33f5ded8f42
evaldo <evaldo.junior@etec.sp.gov.br>
2023-11-23 10:55:35 -0300

    Validação do formulário de emprea.

--- pages/empresa/novo.php
+++ pages/empresa/novo.php
@@ -1,37 +1,62 @@
 <div class="container">
   
-    <form>
-        <div class="mb-3 input-group">
+    <form method="POST" id="formulario"> <!--02 Alterar o método da requisição para POST -->
+        <div class="mb-3 input-group has-validation"> <!-- 06 - incluir a classe de campo com validação has-validation!-->
           <span class="input-group-text">Nome</span>
           <!--Incluir Atributo "name" nos inputs que vão ser enviados para salvar no banco de dados -->        
           <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome da empresa">
+          <!--05 informe uma mensagem de validação do campo nome -->
+          <div class="invalid-feedback">
+            Por favor digite o nome da empresa
+          </div>
+          <!--05 FIM informe uma mensagem de validação do campo nome -->
+
         </div>
-        
+
+         
+        <!--01-   TELEFONE da EMPRESA -->        
+        <div class="mb-3 input-group has-validation "> <!-- 04 - incluir a classe de campo com validação has-validation!-->
+          <span class="input-group-text">Telefone</span>
+          <input type="tel" class="form-control" id="telefone" name="telefone" placeholder="Digite o telefone da empresa">
+          <!--03 informe uma mensagem de validação do campo telefone -->
+          <div class="invalid-feedback">
+            Por favor digite um telefone
+          </div>
+          <!--03 FIM informe uma mensagem de validação do campo telefone -->
+        </div>
+        <!--01-FIM TELEFONE da EMPRESA -->        
+
+
         
         <div class="mb-3 input-group">
         <span class="input-group-text">Estado</span>
-        <select class="form-select" aria-label="Default select example" id="estados" onchange="carregarCidadesIBGE()">
+        <select class="form-select" aria-label="Default select example" id="estados"
+           onchange="carregarCidadesIBGE()">
             <option selected>Selecione um estado</option>
         </select>
+            <div class="invalid-feedback">
+                Por favor selecione um estado
+            </div>
         </div>            
 
         <div class="mb-3 input-group">
         <span class="input-group-text">Cidade</span>
         <select class="form-select" aria-label="Default select example" id="cidade" name="cidade">
-            <option selected>Selecione uma cidade</option>
+            <option selected>Selecione antes um estado</option>
         </select>
+          <div class="invalid-feedback">
+            Por favor selecione uma cidade
+          </div>        
         </div>            
 
-
-
         <div class="mb-3 input-group">
-            
-            <!--Incluir os inputs que serão utilizados para enviar a geolocalização  -->        
             <input type="hidden" id="geolocalizacao" name="geolocalizacao">
-
             <span class="input-group-text">Endereço</span>
             <input type="text" class="form-control" id="endereco" placeholder="Digite o endereço da empresa">
             <button type="button"  onclick="obterCoordenadasGoogleMaps()" class="input-group-text">Localizar</button>
+            <div class="invalid-feedback">
+               Por favor localize a empresa no mapa.
+            </div>            
         </div>
         <?php require 'pages/empresa/mapa.php'; ?>
 
@@ -49,10 +74,12 @@
             .then(response => response.json())
             .then(data => {
                     if (data!=null && data.length>0) {
-
+                        /**ordena o vetor a partir do nome do estado */
+                        let ordenado = data.sort( (a,b)=>{  return a.nome.localeCompare(b.nome)   }   );
                         
                         var selectEstado = document.getElementById("estados");
-                        data.forEach(element => {
+                        /**Utiliza o vetor ordenado para gerar as opções do select */
+                        ordenado.forEach(element => {
                             
                             let option = document.createElement('option');
                             option.value= element['sigla'];
@@ -98,4 +125,49 @@
             });;
         
      }
+
+
+     function  validacao(e){
+        let passou = true; // flag para saber se passou por toda da validação
+        let enome =document.getElementById("nome");
+        if (enome.value==null || enome.value == "") {
+            passou = false;
+            enome.classList.add("is-invalid");
+        } else {
+            enome.classList.remove("is-invalid");
+        }
+
+        let etelefone =document.getElementById("telefone");
+        if (etelefone.value==null || etelefone.value.length <9) {
+            passou = false;
+            etelefone.classList.add("is-invalid");
+        } else {
+            etelefone.classList.remove("is-invalid");
+        }
+
+        let ecidade =document.getElementById("cidade");
+        if (ecidade.value==null || ecidade.value=="") {
+            passou = false;
+            ecidade.classList.add("is-invalid");
+        } else {
+            ecidade.classList.remove("is-invalid");
+        }
+
+        let egeo =document.getElementById("geolocalizacao");
+        let eend =document.getElementById("endereco");
+        if (egeo.value==null || egeo.value.length <=3) {
+            passou = false;
+            eend.classList.add("is-invalid");
+        } else {
+            eend.classList.remove("is-invalid");
+        }
+        
+        if (!passou) {
+           e.preventDefault();
+        }
+        
+     }
+
+     document.getElementById("formulario").addEventListener('submit',validacao);
+
 </script>    
